5cfc54c9a3541ebe054b56e13e3a26324e8a7067
Salman Ahmad <salmanahmad111499@gmail.com>
2024-12-10 22:19:54 +0500

    created media_manager.py

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+# including important libraries
+import media_manager
+
+# MAIN
+if __name__ == "__main__":
+    print("Working")
---
+++ media_manager/media_manager.py
@@ -0,0 +1,12 @@
+
+
+# this file contains Media Manager class
+
+class MediaManager:
+    """Used in handling media related items.
+    """
+
+    # private data mambers
+    # constructor
+    def MediaManager(self):
+        pass
