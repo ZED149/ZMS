8526451fe8bd14be98e89ee85af07364cf0f52c3
Salman Ahmad <salmanahmad111499@gmail.com>
2025-04-29 19:44:21 +0500

    introduced log.py to the module

---
+++ media_manager/log.py
@@ -0,0 +1,18 @@
+
+
+# Created @ 4-Apr-2025 Tuesday
+# Author: Salman Ahmad
+
+'''
+Contains log class for the Media Manager App
+'''
+
+# Log class
+class Log:
+    '''
+    Logs various parameters of Media Manager like while inserting movies or reading emails from Excel file etc.
+    '''
+
+    # Constructor
+    def __init__(self):
+        pass
