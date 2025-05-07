db0a4db83844076157fb1803e989815409808a11
Salman Ahmad <salmanahmad111499@gmail.com>
2025-05-07 16:21:37 +0500

    wrote code for Movie class

--- media_manager/classes/__init__.py
+++ media_manager/classes/__init__.py
@@ -2,3 +2,4 @@
 
 from .tvshow import TVShow
 from .episode import Episode
+from .movie import Movie
--- media_manager/classes/movie.py
+++ media_manager/classes/movie.py
@@ -4,4 +4,25 @@
 
 
 class Movie:
-    pass
+    # data members
+    movie_name = None
+    release_year = None
+    genres = None
+    rating = None
+    small_cover_image = None
+
+    # constructor
+    def __init__(self, movie_name: str = None, release_year: str = None, 
+                 genres: str = None, rating: str = None,
+                 small_cover_image: str = None):
+        # initializing data members
+        self.movie_name = movie_name
+        self.release_year = release_year
+        self.genres = genres
+        self.rating = rating
+        self.small_cover_image = small_cover_image
+
+    # __str__
+    def __str__(self):
+        message = f'[MOVIE] [{self.movie_name}] ({self.release_year}) ({self.rating}/10)'
+        return message
