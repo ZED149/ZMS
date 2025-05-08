8e128aa3865d7ba2e06dda180357c76255fc7ade
Salman Ahmad <salmanahmad111499@gmail.com>
2025-05-08 17:47:22 +0500

    added run_first_time.py to execute only when the script runs for the first time.

---
+++ run_first_time.py
@@ -0,0 +1,41 @@
+
+
+# Run this file only for the first time before executing the main script
+
+
+
+# including important libraries
+from media_manager import MediaManager
+import os
+from dotenv import load_dotenv
+
+# loading enviournmental variables into our scope
+load_dotenv(dotenv_path="media_manager/.env")
+
+# GLOBAL Variables
+DB_NAME = os.getenv('DB_NAME')
+VERBOSITY = True
+EMAIL_FILE = os.getenv("EMAIL_FILE")
+
+# MAIN
+if __name__ == "__main__":
+    print("Script initiated...")
+
+    # Instanciating MediaManager object
+    o = MediaManager(verbosity=VERBOSITY, db_name=DB_NAME, logger_name="Salman Ahmad")
+
+    # setting sending email and commiting to DB flag = False
+    flag = False
+
+    # creating email table in the DB
+    o.cetid(verbosity=VERBOSITY, db_name=DB_NAME)
+    # adding emails to the db
+    o.aetd(verbosity=VERBOSITY, file=EMAIL_FILE, db_name=DB_NAME)
+
+    # creating movies table in the DB
+    o.cmtid(verbosity=VERBOSITY, db_name=DB_NAME)
+    
+    # creating tv_shows table in the DB
+    o.ctstid(verbosity=VERBOSITY, db_name=DB_NAME)
+
+    o.commit()
